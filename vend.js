f1aa4281a2ccaef94addba9becbaf63173035486
Pulkit Singhal <pulkit@shoppinpal.com>
2015-01-09 13:00:59 -0600

    simplify client code and add oauth utility methods

--- vend.js
+++ vend.js
@@ -1,86 +1,131 @@
-var Vend = (function() {
-  'use strict';
+'use strict';
 
-  var Promise = require('bluebird');
-  var _ = require('underscore');
-  var request = require('request-promise');
-  //request.debug = true;
+var _ = require('underscore');
+var log = require('winston');
+var Promise = require('bluebird');
 
-  function Vend(domainPrefix, accessToken) {
-    this.domainPrefix = domainPrefix;
-    this.accessToken = accessToken;
-    this.url = 'https://' + this.domainPrefix + '.vendhq.com';
-    //console.log(this.url);
+var request = require('request-promise');
+//request.debug = true;
+
+function RateLimitingError(e) {
+  return e.statusCode == 429;
+}
+
+function ClientError(e) {
+  return e.statusCode >= 400 && e.statusCode < 500;
+}
+
+var successHandler = function(response) {
+  if(_.isArray(response)) {
+    console.log('response is an array');
+  } else if(_.isObject(response)) {
+    console.log('response is an object');
+  } else if(_.isString(response)) {
+    console.log('response is a string');
+    try{
+      var responseObject = JSON.parse(response);
+      console.log(responseObject);
+      return Promise.resolve(responseObject);
+    } catch(error){
+      console.error('caught an error: ', error);
+      throw error;
+    }
+  } else {
+    console.log(response);
   }
+};
 
-  function RateLimitingError(e) {
-    return e.statusCode == 429;
-  }
+var fetchProducts = function(domainPrefix, accessToken){
+  var path = '/api/products';
+  var vendUrl = 'https://' + domainPrefix + '.vendhq.com' + path;
+  var authString = 'Bearer ' + accessToken;
+  log.debug('GET ' + vendUrl);
+  log.debug('Authorization: ' + authString);
 
-  function ClientError(e) {
-    return e.statusCode >= 400 && e.statusCode < 500;
-  }
+  //var domainPrefix = this.domainPrefix;
 
-  Vend.prototype.fetchProducts = function(parameters){
-    var path = '/api/products';
-    var authString = 'Bearer ' + this.accessToken;
-    var url = this.url + path;
-    //var domainPrefix = this.domainPrefix;
-
-    //console.log('GET ' + url);
-    //console.log('Authorization: ' + authString);
-    var options = {
-      url: url,
-      headers: {
-        'Authorization': authString
-      }
-    };
-
-    return request(options)
-      .then(function(response) {
-        if(_.isArray(response)) {
-          console.log('response is an array');
-        } else if(_.isObject(response)) {
-          console.log('response is an object');
-        } else if(_.isString(response)) {
-          console.log('response is a string');
-          try{
-            var responseObject = JSON.parse(response);
-            console.log('Fetched ' + responseObject.products.length + ' products');
-            return Promise.resolve(responseObject);
-          } catch(error){
-            console.error('caught an error: ', error);
-            throw error;
-          }
-        } else {
-          console.log(response);
-        }
-      })
-      .catch(RateLimitingError, function(e) {
-        console.log('A RateLimitingError error like "429 Too Many Requests" happened: '
-          + e.statusCode + ' ' + e.response.body + '\n'
-          + JSON.stringify(e.response.headers,null,2));
-      })
-      .catch(ClientError, function(e) {
-        console.log('A ClientError happened: '
-          + e.statusCode + ' ' + e.response.body + '\n'
-          /*+ JSON.stringify(e.response.headers,null,2)
-          + JSON.stringify(e,null,2)*/
-        );
-        // TODO: add retry logic
-        //       perhaps use: https://github.com/you21979/node-limit-request-promise
-        /*Promise.delay(3000)
-          .then(function() {
-            console.log("3000 ms passed");
-            return new Vend(domainPrefix).fetchProducts(parameters);
-          });*/
-      })
-      .catch(function(e) {
-        console.error('An unexpected error occurred: ', e);
-      });
+  var options = {
+    url: vendUrl,
+    headers: {
+      'Authorization': authString
+    }
   };
 
-  return Vend;
-})();
+  return request(options)
+    .then(successHandler)
+    .catch(RateLimitingError, function(e) {
+      console.log('A RateLimitingError error like "429 Too Many Requests" happened: '
+        + e.statusCode + ' ' + e.response.body + '\n'
+        + JSON.stringify(e.response.headers,null,2));
+    })
+    .catch(ClientError, function(e) {
+      console.log('A ClientError happened: '
+        + e.statusCode + ' ' + e.response.body + '\n'
+        /*+ JSON.stringify(e.response.headers,null,2)
+        + JSON.stringify(e,null,2)*/
+      );
+      // TODO: add retry logic
+      //       perhaps use: https://github.com/you21979/node-limit-request-promise
+      /*Promise.delay(3000)
+        .then(function() {
+          console.log("3000 ms passed");
+          return new Vend(domainPrefix).fetchProducts(parameters);
+        });*/
+    })
+    .catch(function(e) {
+      console.error('An unexpected error occurred: ', e);
+    });
+};
 
-exports.Vend = Vend;
+var getInitialAccessToken = function(tokenUrl, clientId, clientSecret, redirectUri, code, domainPrefix, state) {
+  // TODO: tweak winston logs to prefix method signature (like breadcrumbs) when logging?
+  log.debug('getInitialAccessToken - token Url: '+ tokenUrl);
+  log.debug('getInitialAccessToken - client Id: ' + clientId);
+  log.debug('getInitialAccessToken - client Secret: ' + clientSecret);
+  log.debug('getInitialAccessToken - redirect Uri: ' +  redirectUri);
+  log.debug('getInitialAccessToken - code: ' + code);
+  log.debug('getInitialAccessToken - domain_prefix: ' + domainPrefix);
+  log.debug('getInitialAccessToken - state: ' + state);
+
+  var options = {
+    url: tokenUrl,
+    headers: {
+      'Accept': 'application/json'
+    },
+    form:{
+      'code': code,
+      'client_id': clientId,
+      'client_secret': clientSecret,
+      'grant_type': 'authorization_code',
+      'state': state,
+      'redirect_uri': redirectUri
+    }
+  };
+  return request.post(options)
+    .then(successHandler)
+    .catch(RateLimitingError, function(e) {
+      console.log('A RateLimitingError error like "429 Too Many Requests" happened: '
+        + e.statusCode + ' ' + e.response.body + '\n'
+        + JSON.stringify(e.response.headers,null,2));
+    })
+    .catch(ClientError, function(e) {
+      console.log('A ClientError happened: '
+          + e.statusCode + ' ' + e.response.body + '\n'
+        /*+ JSON.stringify(e.response.headers,null,2)
+         + JSON.stringify(e,null,2)*/
+      );
+      // TODO: add retry logic
+      //       perhaps use: https://github.com/you21979/node-limit-request-promise
+      /*Promise.delay(3000)
+       .then(function() {
+       console.log("3000 ms passed");
+       return new Vend(domainPrefix).fetchProducts(parameters);
+       });*/
+    })
+    .catch(function(e) {
+      console.error('An unexpected error occurred: ', e);
+    });
+};
+
+exports.fetchProducts = fetchProducts;
+exports.getInitialAccessToken = getInitialAccessToken;
